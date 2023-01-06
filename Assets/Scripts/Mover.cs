9ef8606158b061c53b58138eba987dda8041bdfa
Maurice Okumu <maurice@Maurices-MacBook-Pro.local>
2023-01-06 12:44:54 -0600

    Add all movement to tank

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -5,29 +5,35 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     // serialize to access the variable in the editor
-    [SerializeField] float moveSpeed = 1.7f;
+    [SerializeField] float moveSpeed = 0.1f;
     [SerializeField] float turnSpeed = 19.1f;
-    // Start is called before the first frame update
     void Start()
     {
        
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float roll = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-        transform.Translate(roll, 0, 0);
+        MoveTank();
         TurnTank();
         
         
     }
+    void MoveTank ()
+    {
+        float roll = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        transform.Translate(roll, 0, 0);
+    }
 
     void TurnTank ()
     {
-    //    if (Input.GetKeyDown(KeyCode.UpArrow))
-    //     {
+       if (Input.GetKey(KeyCode.UpArrow))
+        {
         transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime);
-        // }
+        }
+       if (Input.GetKey(KeyCode.DownArrow))
+        {
+        transform.Rotate(Vector3.up * -turnSpeed * Time.deltaTime);
+        }
     }
 }
